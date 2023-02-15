621f84a4c892bb8def7a5ecc6e0e0312f090772f
Marcos Gomes <marcosgomesdev@gmail.com>
2023-02-15 18:58:15 -0300

    Creating differents routes by authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   resources :friends
   # get 'home/index'
   get 'home/about'
-  root 'home#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'friends#index', as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root 'home#index', as: :unauthenticated_root
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
